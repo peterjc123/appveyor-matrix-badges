c8c62ab82edbd8573f6478853aa15af44b29b1e5
Tzach Shabtay <tzachshabtay@gmail.com>
2017-01-18 22:30:25 -0500

    Added no-caching to prevent github from caching an old build status and added pending states

--- index.js
+++ index.js
@@ -3,20 +3,18 @@ var http = require("http");
 var https = require("https");
 var request = require('request');
 
-
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 //  /^\/(.+)/
-app.get("/repos/(*)", function(req, res) {
+app.get("/repos/(*)", function (req, res) {
   var self = this;
   var repo = req.params[0];
   var requestedJobNumber = "";
   var tokens = repo.split("/");
   requestedJobNumber = tokens[tokens.length - 1];
-  if (requestedJobNumber == "")
-  {
+  if (requestedJobNumber == "") {
     res.status(400);
     url = req.url;
     res.send('Job number is missing from the url');
@@ -24,48 +22,62 @@ app.get("/repos/(*)", function(req, res) {
   }
   repo = repo.slice(0, repo.lastIndexOf("/"));
   //console.log(repo);
-   var options = {
-    url: "https://ci.appveyor.com/api/projects/" + repo
-   };
-request(options, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    //console.log(body); 
-    var json = JSON.parse(body);
-    var jobs = json.build.jobs;
-    if(!jobs){
-      res.status(400);
-      url = req.url;
-      res.send('Jobs were not found for build');
-    }
-    
-    var foundRequestedJobNumber = false;
-    var currentJobNumber = 0;
-    jobs.forEach(function(job) {
+  var options =
+    {
+      url: "https://ci.appveyor.com/api/projects/" + repo
+    };
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      //console.log(body); 
+      var json = JSON.parse(body);
+      var jobs = json.build.jobs;
+      if (!jobs) {
+        res.status(400);
+        url = req.url;
+        res.send('Jobs were not found for build');
+      }
+
+      var foundRequestedJobNumber = false;
+      var currentJobNumber = 0;
+      jobs.forEach(function (job) {
         currentJobNumber++;
         var state = job.status;
         if (requestedJobNumber != currentJobNumber) return;
         foundRequestedJobNumber = true;
-        res.redirect(state == "success" ? 
-        "https://img.shields.io/badge/build-passing-brightgreen.svg" :
-        "https://img.shields.io/badge/build-failure-red.svg");
-    });
+        res.header("Cache-Control", "no-cache");
+        if (state == "success") {
+          redirect("https://img.shields.io/badge/build-passing-brightgreen.svg", state, res)
+        }
+        else if (state == "failed") {
+          redirect("https://img.shields.io/badge/build-failure-red.svg", state, res);
+        }
+        else {
+          var url = "https://img.shields.io/badge/build-" + state + "-yellow.svg";
+          redirect(url, state, res);
+        }
+      });
       if (foundRequestedJobNumber) return;
       res.status(400);
       url = req.url;
       res.send('Job Number was not found');
-    } 
+    }
 
     else {
-    res.status(400);
-    url = req.url;
-    res.send('Build was not found');
+      res.status(400);
+      url = req.url;
+      res.send('Build was not found');
     }
-}) 
-   
-  
-
+  })
 });
-  
-app.listen(app.get('port'), function() {
+
+function redirect(url, state, res) {
+  res.header("ETag", state);
+  request.get(url, function(err, response, body) {
+    if (err) res.send(err);
+    else res.send(body);
+  });
+}
+
+app.listen(app.get('port'), function () {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
